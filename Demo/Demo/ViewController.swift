10034c8df1f151df4997344c8e8bf97c819eec54
= <qwstnz@gmail.com>
2016-06-23 22:15:33 -0400

    ground zero

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by George on 2016-06-23.
+//  Copyright © 2016 George Kye. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+  
+  var label:UILabel
+  
+  init(frame: CGRect, labelText: String) {
+    label               = UILabel()
+    label.font          = UIFont.boldSystemFontOfSize(24)
+    label.text          = labelText
+    label.textAlignment = NSTextAlignment.Center
+    super.init(frame: frame)
+    
+    self.addSubview(self.label)
+    self.backgroundColor    = UIColor.whiteColor()
+  }
+  
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    self.label.frame = self.bounds
+  }
+  
+}
+
+class ViewController: UIViewController, AutoScrollingViewDataSource, AutoScrollingViewDelegate {
+  
+  
+  var imgs = ["0", "1", "2", "3"]
+  @IBOutlet var imageV: UIImageView!
+  var asview: AutoScrollingView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    asview = AutoScrollingView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 300))
+    asview.dataSource = self
+    asview.delegate = self
+    self.view.addSubview(asview)
+  }
+  
+  
+  func createCustomViews()->[UIView]{
+    var array: [UIView] = []
+    for index in 0...3 {
+     array.append(CardView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 350), labelText: "\(index)"))
+    }
+    return array
+  }
+  
+  func createImageViews()->[UIImageView]{
+    var array: [UIImageView] = []
+    for index in 0...3 {
+      let img = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 300))
+      img.image = UIImage(named: "\(index)")
+      array.append(img)
+    }
+    return array
+  }
+  
+  func setAutoScrollingViewDataSource(autoScrollingView: AutoScrollingView) -> [UIView] {
+    return createImageViews()
+  }
+  
+  @IBAction func pauseScrolling(){
+    asview.pauseScrolling()
+  }
+  
+  @IBAction func startScrolling(){
+    asview.startScrolling()
+  }
+
+  //MAKR: ASView delegates
+  
+  func autoScrollingView(autoScrollingView: AutoScrollingView, didSelectItem index: Int) {
+    imageV.image = UIImage(named: imgs[index])
+    print("did select")
+
+  }
+  
+  func autoScrollingView(autoScrollingView: AutoScrollingView, didChangeStatus status: ScrollingState) {
+    print(status.rawValue)
+  }
+  
+  func autoScrollingView(autoScrollingView: AutoScrollingView, didAutoScroll index: Int) {
+    print("auto scrolling at index ", index)
+  }
+  
+  
+}
+
---
+++ Demo/Source/AutoScrollingView.swift
@@ -0,0 +1,194 @@
+//
+//  AutoScrollingView.swift
+//
+//  Created by George on 2016-06-23.
+//  Copyright © 2016 George Kye. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+private class ASViewCell: UICollectionViewCell{
+  
+  init() {
+    super.init(frame: CGRectZero)
+  }
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+}
+
+//MARK: Protocols
+
+@objc public protocol AutoScrollingViewDataSource: class{
+  optional func setAutoScrollingViewDataSource(autoScrollingView: AutoScrollingView)->[UIView]
+  
+}
+
+@objc public protocol AutoScrollingViewDelegate: class {
+  optional func autoScrollingView(autoScrollingView: AutoScrollingView, didAutoScroll index: Int)
+  optional func autoScrollingView(autoScrollingView: AutoScrollingView, didSelectItem index: Int)
+  optional func autoScrollingView(autoScrollingView: AutoScrollingView, didChangeStatus status: ScrollingState)
+}
+
+@objc public enum ScrollingState: Int {
+  case  NotPaused, Paused
+}
+
+
+@IBDesignable public class AutoScrollingView: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate{
+  
+  public weak var delegate: AutoScrollingViewDelegate?
+  
+  public weak var dataSource: AutoScrollingViewDataSource? {
+    didSet {
+      if let views = dataSource?.setAutoScrollingViewDataSource?(self){
+        dataSourceViews = views
+        collectionView.reloadData()
+      }
+    }
+  }
+  
+  //MARK: private
+  private var dataSourceViews = [UIView]()
+  
+  private var timer = NSTimer()
+  private var scrollTimer: NSTimer!
+  private var currentIndex: Int = 0
+  private var timerPaused: Bool = false
+  
+
+  //MARK: public
+  public var timerInterval: Double = 2 {
+    didSet{
+      setupTimer()
+      if timerPaused {
+        self.pauseScrolling()
+      }else { self.startScrolling() }
+    }
+  }
+  
+  
+  lazy var collectionView: UICollectionView = {
+    let layout:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    layout.itemSize = self.frame.size
+    layout.minimumInteritemSpacing = 0
+    layout.minimumLineSpacing = 0
+    layout.scrollDirection = .Horizontal
+    let collectionView = UICollectionView(frame: self.frame, collectionViewLayout: layout)
+    collectionView.dataSource = self
+    collectionView.delegate = self
+    collectionView.registerClass(ASViewCell.self, forCellWithReuseIdentifier: "ASViewCell")
+    collectionView.showsVerticalScrollIndicator = false
+    collectionView.showsHorizontalScrollIndicator = false
+    collectionView.pagingEnabled = true
+    return collectionView
+  }()
+  
+  private func initialize() {
+    collectionView.frame = self.frame
+    collectionView.removeFromSuperview()
+    self.addSubview(self.collectionView)
+    setupTimer()
+  }
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.initialize()
+  }
+  
+  required public init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+  
+  //MARK: AutoScroll (NSTimer)
+  
+  @objc private func autoScrollView(){
+    print("index is \(currentIndex)")
+    let indexPath = NSIndexPath(forRow: currentIndex, inSection: 0)
+    collectionView.scrollToItemAtIndexPath(indexPath, atScrollPosition: .None, animated: true)
+    delegate?.autoScrollingView?(self, didAutoScroll: currentIndex)
+    currentIndex += 1
+    if currentIndex == dataSourceViews.count{
+      currentIndex = 0
+    }
+  }
+  
+  private func setupTimer(){
+    timer.invalidate()
+    timer = NSTimer.scheduledTimerWithTimeInterval(timerInterval, target: self, selector: #selector(self.autoScrollView), userInfo: nil, repeats: true)
+  }
+  
+  public func pauseScrolling(){
+    timer.invalidate()
+    delegate?.autoScrollingView?(self, didChangeStatus: .Paused)
+  }
+  
+  public func startScrolling(){
+    setupTimer()
+    delegate?.autoScrollingView?(self, didChangeStatus: .NotPaused)
+  }
+  
+  
+  //MARK: CollectionView DataSource
+  
+  public func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return dataSourceViews.count
+  }
+  
+  public func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ASViewCell", forIndexPath: indexPath) as! ASViewCell
+    let view = dataSourceViews[indexPath.row]
+    cell.contentView.addSubview(view)
+    
+    return cell
+    
+  }
+  
+  public func collectionView(collectionView: UICollectionView, willDisplayCell cell: UICollectionViewCell, forItemAtIndexPath indexPath: NSIndexPath) {
+    currentIndex = indexPath.row
+  }
+  
+  //MARK: CollectionView Delegate
+  
+  public func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    delegate?.autoScrollingView?(self, didSelectItem: indexPath.row)
+  }
+  
+  //MARK: UIScrollView delegate method
+  public func scrollViewWillBeginDragging(scrollView: UIScrollView)
+  {
+    pauseScrolling()
+  }
+  
+  public func scrollViewDidEndDragging(scrollView: UIScrollView, willDecelerate decelerate: Bool)
+  {
+    startScrolling()
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
